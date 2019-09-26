7204d5365bf102c23bfff4f9a729e14c78a0c485
Mingjie <xuming1@ecs.ac.nz>
2019-09-26 23:31:20 +1200

    Json Parser (Loader and Saver) been created and implemented.

--- JsonParser/src/Persistence/JsonSaver.java
+++ JsonParser/src/Persistence/JsonSaver.java
@@ -1,24 +1,11 @@
 package Persistence;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import java.io.*;
 import java.util.Map;
 
 public class JsonSaver {
 
-    public static void makeJson(Game game) {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-        objectBuilder.add("BoardArray", game.board.toString())
-                .add("Chap", game.getCharacters().toString())
-                .add("Time", game.getBoard().toString());
-
-        JsonObject jsonObject = objectBuilder.build();
-        String jsonString = "";
-        Writer writer = new StringWriter();
-        Json.createWriter(writer).write(jsonObject);
-        jsonString = writer.toString();
 
         try {
             writeToFile(jsonString);
